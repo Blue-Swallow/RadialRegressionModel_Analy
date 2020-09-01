71b06be502e5e373066173e66a24c2f9ad6e459c
Blue-Swarrow <ayumu.colossians2.6@gmail.com>
2020-09-01 22:01:43 +0900

    first commit

---
+++ mod_analy.py
@@ -0,0 +1,212 @@
+# coding -*- utf-8 -*-
+""" 
+Autor: Ayumu Tsuji
+Date: 20200821
+
+Description:
+This file is a module for analyzing fuel regression shape of single port.
+And calculate several proportional constant and exponet constants of
+radial fuel regrerssion model, which is resemble with conventional
+cylindrical hybrid rocket.
+"""
+
+import os
+import json
+import warnings
+import sys
+import matplotlib
+import numpy as np
+import pandas as pd
+from scipy import interpolate
+from scipy import optimize
+from sklearn import metrics
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+import mod_steady_shape
+from tqdm import tqdm
+
+matplotlib.style.use("tj_origin.mplstyle")
+
+class Fitting:
+    def __init__(self, **kwargs):
+        self.param = kwargs
+        if "fldlist" in kwargs:
+            self.fldlist = kwargs["fldlist"]
+        else:
+            self.fldlist = self._get_fldlist_()
+        self.exp_cond, self.shape_dat = self._read_dat_(self.fldlist)
+
+    def _get_fldlist_(self):
+        fldlist = os.listdir(path="exp_dat")
+        return fldlist
+
+    def _read_dat_(self, fldlist):
+        exp_cond = {}
+        shape_dat = {}
+        for fldname in fldlist:
+            with open(os.path.join("exp_dat", fldname, "exp_cond.json")) as f:
+                tmp = json.load(f)
+                tmp["Pc"] = tmp["Pc"]*1e+6      # convert unit MPa to Pa
+                tmp["d"] = tmp["d"]*1e-3        # convert unit mm to m
+                exp_cond[fldname] = tmp
+            tmp = pd.read_csv(os.path.join("exp_dat", fldname, "shape_dat.csv"), header=0, skiprows=[1,])
+            tmp.x = tmp.x*1e-3  # convert unit mm to m
+            tmp.r = tmp.r*1e-3  # convert unit mm to m
+            shape_dat[fldname] = tmp
+        return exp_cond, shape_dat
+
+    def get_R_R2_mean(self, Cr, z, m, mode="R2"):
+        coef = np.array([])
+        for testname, exp_cond in self.exp_cond.items():
+            x = np.array(self.shape_dat[testname].x)
+            r = np.array(self.shape_dat[testname].r)
+            Pc = exp_cond["Pc"]
+            Vox = exp_cond["Vox"]
+            d = exp_cond["d"]
+            tmp = self.get_R_R2(x, r, Pc, Vox, d, Cr, z, m, mode=mode)
+            coef = np.append(coef, tmp)
+        return coef.mean()
+
+    def get_R_R2(self, x, r, Pc, Vox, d, Cr, z, m, mode="R2"):
+        inst = mod_steady_shape.Main(Pc, Vox, **self.param, d=d, Cr=Cr, z=z, m=m)
+        x_model, r_model, rdot_model = inst.exe()
+        func_interp = interpolate.CubicSpline(x_model, r_model, bc_type="natural", extrapolate=None)
+        r_interp = np.array([func_interp(val) for val in x])
+        if mode is "R2":    # calaculate coefficient of derming factor, R2.
+            coefficient = metrics.r2_score(r, r_interp)
+        elif mode is "R":   # calculate correlate coefficient, R.
+            coefficient = np.corrcoef(r, r_interp)[0][1]
+        else:
+            warnings.warn("Please assign proper keyword argument. Not \"{}\" but \"R\" or \"R2\"".format(mode))
+            sys.exit()
+        return coefficient
+
+    def optimize_modelconst(self, mode="R2", **kwargs):
+        if "Cr_init" in kwargs:
+            Cr_init = kwargs["Cr_init"]
+        else:
+            Cr_init = self.param["Cr_init"]
+        if "z_init" in kwargs:
+            z_init = kwargs["z_init"]
+        else:
+            z_init = self.param["z_init"]
+        if "m_init" in kwargs:
+            m_init = kwargs["m_init"]
+        else:
+            m_init = self.param["m_init"]
+        init_const = np.array([Cr_init, z_init, m_init])
+        func_opt = lambda const, mode: -1*self.get_R_R2_mean(const[0], const[1], const[2], mode=mode)
+        if "method" in kwargs:
+            method = kwargs["method"]
+            if method is "global":      # optimization for seeking global mimimum
+                bounds = kwargs["bounds"]
+                res = optimize.differential_evolution(func_opt, bounds=bounds, args=(mode,))
+            else:                       # optimizatioin for seeking local minimum with assigning optimization method
+                res = optimize.minimize(func_opt, x0=init_const, args=(mode,), method=method)
+        else:                           # optimizaiton for seeking local minimum
+            res = optimize.minimize(func_opt, x0=init_const, args=(mode,))
+        return res
+    
+    def plot_R_R2(self, bounds, mode="R2", resolution=10, thirdparam="Cr", num_fig=9, **kwargs):
+        bound_Cr = bounds[0]
+        bound_z = bounds[1]
+        bound_m = bounds[2]
+        if thirdparam == "Cr":
+            interb_z = (bound_z[1] - bound_z[0])/resolution
+            x_array = np.arange(bound_z[0], bound_z[1]+interb_z/2, interb_z)
+            interb_m = (bound_m[1] - bound_m[0])/resolution
+            y_array = np.arange(bound_m[0], bound_m[1]+interb_m/2, interb_m)
+            if num_fig == 1:
+                third_array = np.array([(bound_Cr[0]+bound_Cr[1])/2])
+            else:
+                interb_Cr = (bound_Cr[1] - bound_Cr[0])/(num_fig-1)
+                third_array = np.arange(bound_Cr[0], bound_Cr[1]+interb_Cr/2, interb_Cr)
+        elif thirdparam == "z":
+            if num_fig == 1:
+               third_array = np.array([(bound_z[0]+bound_z[1])/2])
+            else:
+                interb_z = (bound_z[1] - bound_z[0])/(num_fig-1)
+                third_array = np.arange(bound_z[0], bound_z[1]+interb_z/2, interb_z)
+            interb_m = (bound_m[1] - bound_m[0])/resolution
+            x_array = np.arange(bound_m[0], bound_m[1]+interb_m/2, interb_m)
+            interb_Cr = (bound_Cr[1] - bound_Cr[0])/resolution
+            y_array = np.arange(bound_Cr[0], bound_Cr[1]+interb_Cr/2, interb_Cr)
+        elif thirdparam == "m":
+            interb_z = (bound_z[1] - bound_z[0])/resolution
+            x_array = np.arange(bound_z[0], bound_z[1]+interb_z/2, interb_z)
+            if num_fig == 1:
+               third_array = np.array([(bound_m[0]+bound_m[1])/2])
+            else:
+                interb_m = (bound_m[1] - bound_m[0])/(num_fig-1)
+                third_array = np.arange(bound_m[0], bound_m[1]+interb_m/2, interb_m)
+            interb_Cr = (bound_Cr[1] - bound_Cr[0])/resolution
+            y_array = np.arange(bound_Cr[0], bound_Cr[1]+interb_Cr/2, interb_Cr)
+        else:
+            print("There is no such a parameter thirdparam=\"{}\"".format(thirdparam))
+            sys.exit()
+        z = np.empty((num_fig, resolution+1, resolution+1))
+        for i in tqdm(range(len(third_array)), desc="Total Progress", leave=True):
+            for j in tqdm(range(len(x_array)), desc="Cr={}".format(third_array[i]), leave=False):
+                for k in range(len(y_array)):
+                    if thirdparam is "Cr":
+                        coef = self.get_R_R2_mean(Cr=third_array[i], z=x_array[j], m=y_array[k], mode=mode)
+                    elif thirdparam is "z":
+                        coef = self.get_R_R2_mean(Cr=y_array[k], z=third_array[i], m=x_array[j], mode=mode)
+                    else:
+                        coef = self.get_R_R2_mean(Cr=y_array[j], z=x_array[j], m=third_array[i], mode=mode)
+                    z[i, j, k] = coef
+
+        self._plot_(x_array, y_array, z, thirdparam="Cr", num_fig=num_fig)
+        return z
+
+    def _plot_(self, x, y, z, thirdparam="Cr", num_fig=9):
+        fig = plt.figure(figsize=(24,24))
+        ax = fig.add_subplot(1,1,1, projection="3d")
+        # ax = [0 for i in range(num_fig)]
+        # for i in range(num_fig):
+        #     ax[i] = fig.add_subplot(num_fig, num_fig, i)
+        X, Y = np.meshgrid(x, y)
+        ax.plot_surface(X, Y, z[0], alpha=0.3, vmin=0.0, vmax=1.0, cmap=cm.cividis)
+        ax.contour(X, Y, z[0], levels=10, vmin=0.0, vmax=1.0, cmap=cm.cividis)
+        ax.set_xlim(X.min(), X.max())
+        ax.set_ylim(Y.min(), Y.max())
+        ax.set_zlim(0, 1.0)
+        fig.savefig("test.png", dpi=400)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    PARAM = {"rho_f": 1190,     # [kg/m^3] solid fuel density
+             "M_ox": 32.0e-3,   # [kg/mol]
+             "T": 300,          # [K] oxidizer tempreature
+             "Cr_init": 15e-6,# Initial guess value for coefficent fitting
+             "z_init": 0.4,     # Initial guess value for coefficent fitting
+             "m_init": -0.26,   # Initial guess value for coefficent fitting
+             "C1": 1.39e-7,  # experimental constant of experimental regression rate formula
+             "C2": 1.61e-9,  # experimental constant of experimental regression rate formula
+             "n": 1.0,       # experimental exponent constant of pressure
+             "dx": 0.1e-3,      # [m] space resolution
+             "x_max": 12.6e-3,  # [m] maximum calculation region
+             "r_0": 0.0,        # r=0.0 when x = 0, boudanry condition
+             "rdot_0": 0.0,     # rdot=0 when x = 0, boundary condition
+             "Vf_mode": False   # mode selection using axial or radial integretioin for mf calculation
+            }
+    inst = Fitting(**PARAM)
+    Cr = 15.0e-6
+    z = 0.4
+    m = -0.26
+    coef = inst.get_R_R2_mean(Cr, z, m, mode="R2")
+    print("Coefficient = {}".format(coef))
+    z_array = inst.plot_R_R2(bounds=[(14e-6, 16e-6), (0.2, 0.6), (-0.4, -0.1)], mode="R2", resolution=10, thirdparam="Cr", num_fig=1)
+    print(z_array)
+    # res = inst.optimize_modelconst(mode="R2", method="TNC")
+    # res = inst.optimize_modelconst(mode="R2", method="global", bounds=[(1.0e-6, 30.0e-6), (0.0, 1.0), (-0.5, 0.0)])
+    # res = inst.optimize_modelconst(mode="R", method="global", bounds=[(1.0e-6, 100.0e-6), (0.0, 1.0), (-0.5, 0.0)])
+    # print("Cr={}, z={}, m={}, R2={}".format(res.x[0], res.x[1], res.x[2], -res.fun))
---
+++ mod_steady_shape.py
@@ -0,0 +1,251 @@
+
+# coding: utf-8
+"""
+# Content: Fuel Regression Shape of EBHR after Steady State
+# Author: Ayumu Tsuji @Hokkaido University
+
+# Description:
+Estimate the single port fuel regression shape of axial-injection end-burning
+hybrid rocket when the combustion state becomes steady state.
+To estimate the shape, this program needs some empirical constant,Cr; z; m, k,
+which are obtained by experimental results.
+"""
+
+import numpy as np
+import os
+import json
+import matplotlib.pyplot as plt
+import matplotlib.style as mplstyle
+from scipy.integrate import simps
+from scipy.integrate import trapz
+from scipy.optimize import newton
+from datetime import datetime
+from tqdm import tqdm
+
+mplstyle.use("tj_origin.mplstyle")
+
+
+
+#%% 関数定義
+class Main:
+    def __init__(self, Pc, Vox, **cond):
+        self.Pc = Pc
+        self.Vox = Vox
+        self.cond = cond
+        self.x, self.r, self.rdot = self.initialize()
+
+    def func_Vf(self, Vox, Pc):
+        C1 = self.cond["C1"]
+        C2 = self.cond["C2"]
+        n = self.cond["n"]
+        Vf = (C1/Vox + C2)*np.power(Pc, n)
+        return(Vf)
+
+    def func_mf(self, i, **kwargs):
+        r_0 = self.cond["r_0"]
+        rdot_0 = self.cond["rdot_0"]
+        d = self.cond["d"]
+        rho_f = self.cond["rho_f"]
+        if i == 0:
+            x_range = np.array([0.0])
+            r_range = np.array([r_0])
+            rdot_range = np.array([rdot_0])
+        else:
+            x_range = self.x[:i]
+            r_range = self.r[:i]
+            rdot_range = self.rdot[:i]
+        if self.cond["Vf_mode"]:
+            Vf = self.func_Vf(self.Vox, self.Pc)
+            mf = rho_f*Vf*np.pi*(np.power(self.r[i], 2) +d*self.r[i] )  # calculate using integretion for radial direction
+        else:
+            mf = 2*np.pi*rho_f*simps((r_range + d/2)*rdot_range, x_range)  # calculate using integretion for axial direction
+        return(mf)
+
+    def func_mox(self, i):
+        d = self.cond["d"]
+        M_ox = self.cond["M_ox"]
+        T = self.cond["T"]
+        Rstr = 8.3144598 # [J/mol-K]
+        rho_ox = self.Pc/((Rstr/M_ox)*T)
+        mox = rho_ox*self.Vox*(np.pi*np.power(d,2)/4)
+        return(mox)
+
+    def func_G(self, i, ri):
+        d = self.cond["d"]
+        G = (self.func_mf(i) + self.func_mox(i)) / (np.pi*np.power(2*ri+d, 2)/4)
+        return(G)
+
+    def func_rdot(self, i, ri):
+        G = self.func_G(i, ri)
+        Cr = self.cond["Cr"]
+        z = self.cond["z"]
+        m = self.cond["m"]
+        # k = self.cond["k"]
+        rdot_0 = self.cond["rdot_0"]
+        if self.x[i] == 0:
+            rdot0 = rdot_0
+        else:
+            rdot0 = Cr*np.power(G, z)*np.power(self.x[i], m) # origin formula
+        # # th = k*np.power(G, 0.8)/self.Pc       # equation obtained from dissertation written by Hashimoto
+        # # rdoti = rdot0 *np.sqrt(2/th) *np.sqrt(1 -1/th*(1 -np.exp(-th)))
+        rdoti = rdot0
+        return(rdoti)
+
+    def func_r(self, i, rdoti):
+        rdot_0 = self.cond["rdot_0"]
+        if i == 0:
+            x_range = np.array([0.0])
+            rdot_range = np.array([rdot_0])
+        else:
+            rdot_range = self.rdot[:i]
+            rdot_range = np.append(rdot_range, rdoti)
+            x_range = np.array(self.x[:i])
+            x_range = np.append(x_range, self.x[i])
+        ri = trapz(rdot_range, x_range)/self.func_Vf(self.Vox, self.Pc)
+        return(ri)
+
+    def error_rdot(self, rdoti, i):
+        ri = self.func_r(i, rdoti)
+        rdot_cal = self.func_rdot(i, ri)
+        error = (rdoti - rdot_cal)/rdot_cal
+        return(error)
+    
+    def iterat_rdot(self, i):
+        r_0 = self.cond["r_0"]
+        if i == 0:
+            rdoti = self.func_rdot(i, r_0)
+        else:
+            rdoti = newton(self.error_rdot, x0=self.rdot[i-1] ,args=(i,))
+        return(rdoti)
+
+    def initialize(self):
+        """Generate variable array
+        
+        Parameters
+        ----------
+        **kwargs: keyword variable
+            **kwargs must include the follwoing key; "dx", "x_max", "r_0", "rdot_0"
+
+        Returns
+        -------
+        x: 1d-ndarray of float
+            position [m]
+        r: 1d-ndarray of float
+            initialized fuel regression distance array
+        rdot 1d-ndarray of float
+            initialized fuel regression rate array
+        """
+        dx = self.cond["dx"]
+        x_max = self.cond["x_max"]
+        r_0 = self.cond["r_0"]
+        rdot_0 = self.cond["rdot_0"]
+        x = np.arange(0.0, x_max+dx, dx)
+        r = np.zeros(int(round((x_max+dx)/dx, 0)), float)
+        rdot = np.zeros(int(round((x_max+dx)/dx)), float)
+        r = np.array([r_0 for i in r])
+        rdot = np.array([rdot_0 for i in rdot])
+        return x, r, rdot
+
+    def exe(self):
+        self.x, self.r, self.rdot = self.initialize()
+        for i in range(len(self.r)):
+            self.rdot[i] = self.iterat_rdot(i)
+            self.r[i] = self.func_r(i, self.rdot[i])
+        return self.x, self.r, self.rdot
+
+def plot_r(ax1, x, r, Pc, Vox, mf, **cond):
+    x_max = cond["x_max"]
+    y_max = cond["y_max_r"]
+    # legend_text = r"$P_c$=" + str(round(Pc*1e-6,2)) + " MPa, "\
+    #              + r"$V_{ox}$=" + str(round(Vox, 1)) + " m/s, "\
+    #              + r"$\dot m_f$=" + str(round(mf*1e+6, 1)) + " mg/s"
+    legend_text = r"$P_c$=" + str(round(Pc*1e-6,2)) + " MPa"
+    ax1.plot(x*1.0e+3, r*1.0e+3, label=legend_text)
+    ax1.set_xlabel("Axial distance $x$ [mm]")
+    ax1.set_ylabel("Radial regression distance $r$ [mm]")
+    # ax1.text(x_max*1.0e+3*0.8, y_max*1.0e+3*0.9, r"$P_c$={} MPa".format(Pc*1.0e-6))
+    # ax1.text(x_max*1.0e+3*0.8, y_max*1.0e+3*0.8, r"$Vox$={} m/s".format(Vox))
+    ax1.legend(fontsize=24)
+    ax1.set_ylim(0,y_max*1.0e+3)
+    # ax1.set_xlim(0,x_max*1.0e+3)
+    ax1.set_xlim(0, 15.0)
+    ax1.grid()
+    return ax1
+    # fig1.savefig(os.path.joint(fldname, "r_steady.png"))
+    # fig1.show()
+
+def plot_rdot(ax2, x, rdot, Pc, Vox, **cond):
+    x_max = cond["x_max"]
+    y_max = cond["y_max_rdot"]
+    legend_text = r"$P_c$=" + str(round(Pc*1e-6,2)) + " MPa," + r"$V_{ox}$=" + str(round(Vox, 1)) + " m/s"
+    ax2.plot(x*1.0e+3, rdot*1.0e+3, label=legend_text)
+    ax2.set_xlabel("Axial distance $x$ [mm]")
+    ax2.set_ylabel("Radial regression rate $\dot r$ [mm/s]")
+    #plt.text(x_max*1.0e+3*0.8, 1.0, r"$P_c$={} MPa".format(Pc*1.0e-6))
+    #plt.text(x_max*1.0e+3*0.8, 0.9, r"$Vox$={} m/s".format(Vox))
+    ax2.legend()
+    ax2.set_ylim(0, y_max*1.0e+3)
+    ax2.set_xlim(0,x_max*1.0e+3)
+    ax2.grid()
+    return ax2
+    # fig2.savefig(os.path.joint(fldname, "rdot_steady.png"))
+    # fig2.show()
+
+
+if __name__ == "__main__":
+#%% 計算パラメータの定義
+    PARAM = {"d": 0.3e-3,       # [m] port diameter
+             "rho_f": 1190,     # [kg/m^3] solid fuel density
+             "M_ox": 32.0e-3,   # [kg/mol]
+             "T": 300,          # [K] oxidizer tempreature
+            #  "Cr": 4.58e-6,     
+             "Cr": 3.01e-6,
+            #  "Cr": 20.0e-6,
+             "z": 0.9,
+            # "z": 0.6,
+             "m": -0.2,
+             "k": 3.0e+4,
+             "C1": 1.39e-7,  # experimental constant of experimental regression rate formula
+             "C2": 1.61e-9,  # experimental constant of experimental regression rate formula
+             "n": 1.0,       # experimental exponent constant of pressure
+             "dx": 0.1e-3,      # [m] space resolution
+             "x_max": 30.0e-3,  # [m] maximum calculation region
+             "y_max_r": 1.5e-3,   # [m] maximum plot width of radial regression distance
+             "y_max_rdot": 2.0e-3,   # [m] maximum plot width of radial regression rate
+             "r_0": 0.0,        # r=0.0 when x = 0, boudanry condition
+             "rdot_0": 0.0,     # rdot=0 when x = 0, boundary condition
+             "Vf_mode": False   # mode selection using axial or radial integretioin for mf calculation
+            }
+
+    Pc_range = np.arange(0.25e+6, 1.50e+6, 0.25e+6)
+    Vox_range =  np.arange(20, 81, 10)
+    Vox = 30.0 # [m/s] oxidizer port velocity
+    Pc = 0.25e+6 # [MPa] chamber pressure
+    fig1 = plt.figure()
+    fig2 = plt.figure()
+    fig3 = plt.figure()
+    fig4 = plt.figure()
+    ax1 = fig1.add_subplot(111)
+    ax2 = fig2.add_subplot(111)
+    ax3 = fig3.add_subplot(111)
+    ax4 = fig4.add_subplot(111)
+    fldname = datetime.now().strftime("%Y_%m%d_%H%M%S") + "_SteadyShape"
+    os.makedirs(fldname)
+    with open(os.path.join(fldname, "cond.json"), "w") as f:
+        json.dump(PARAM, f, ensure_ascii=False, indent=4)
+    for Pc_tmp in tqdm(Pc_range):
+        inst1 = Main(Pc_tmp, Vox, **PARAM)
+        x, r, rdot = inst1.exe()
+        mf = inst1.func_mf(inst1.x.size-1)
+        ax1 = plot_r(ax1, x, r, Pc_tmp, Vox, mf, **PARAM)
+        ax2 = plot_rdot(ax2, x, rdot, Pc_tmp, Vox, **PARAM)
+    for Vox_tmp in tqdm(Vox_range):
+        inst2 = Main(Pc, Vox_tmp, **PARAM)
+        x, r, rdot = inst2.exe()
+        mf = inst2.func_mf(inst2.x.size-1)
+        ax3 = plot_r(ax3, x, r, Pc, Vox_tmp, mf, **PARAM)
+        ax4 = plot_rdot(ax4, x, rdot, Pc, Vox_tmp, **PARAM)
+    fig1.savefig(os.path.join(fldname, "r_steady_Pc.png"))
+    fig2.savefig(os.path.join(fldname, "rdot_steady_Pc.png"))
+    fig3.savefig(os.path.join(fldname, "r_steady_Vox.png"))
+    fig4.savefig(os.path.join(fldname, "rdot_steady_Vox.png"))
